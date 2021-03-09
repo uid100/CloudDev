c77884676d66296307fc2daef6e65ae8aa5f5867
Dan Sullivan <daniel.f.sullivan@gmail.com>
2021-03-08 18:10:50 -0800

    Add project files.

---
+++ OfficeWires/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OfficeWires.Models;
+
+namespace OfficeWires.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View(WebApp.GetWebApps());
+        }
+    }
+}
---
+++ OfficeWires/Models/WebApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficeWires.Models
+{
+    public class WebApp
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string URL { get; set; }
+        public string SourceLoc { get; set; }
+        public bool IsRunning { get; set; } = false;
+
+        public static WebApp[] GetWebApps()
+        {
+            return new WebApp[] {
+                new WebApp { 
+                    Name = "Locate",
+                    Description = "GoogleMaps integration demonstration webapp. Display the user's browser location if allowed, or IP location",
+                    URL = "https://findme.azurewebsites.net",
+                    SourceLoc = "https://github.com/uid100/GeoLocateWeb"
+                },
+                new WebApp
+                {
+                    Name = "RateCalculator",
+                    Description = "Find and display GSA per diem rates and other user location details for contract calculations",                 
+                },
+                new WebApp
+                {
+                    Name = "Whiteboard Tech Challenge",
+                },
+                new WebApp
+                {
+                    Name = "CheckedIn",
+                    Description = "Engage Distance Learning students with online prompts. Generates emailed reports and requires integration with Canvas API."
+                },
+            };
+        }
+    }
+}
